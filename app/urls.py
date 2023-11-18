15ddc3164430d3e6ef580185d2bcad6c66c5e47b
amits999 <awesomeamit50@gmail.com>
2023-11-18 21:14:44 +0530

    Getting dynamic urls

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.hello),
-    path('job/<id>', views.job_detail_page)
+    path('', views.hello, name='main_page'),
+    path('job/<int:id>', views.job_detail_page, name='jobs_detail')
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound
+from django.urls import reverse
 
 # Create your views here.
 def hello(request):
     return HttpResponse('Hello World')
 
+job_title = [
+    'Job1 Title',
+    'Job2 Title',
+    'Job3 Title',
+]
+
+job_desc = [
+    'Job1 Description',
+    'Job2 Description',
+    'Job3 Description',
+]
+
+# def job_page(request):
+#     my_str = '<ul>'
+#     for j in job_title:
+#         my_str += f'<a href=<li>{job_title[j.index]}</li>'
+
 def job_detail_page(request, id):
-    return HttpResponse(f'Job detail page: {id}')
+    try:
+        if id == 0:
+            return redirect(reverse('main_page'))
+        
+        return_html = f'<h1>{job_title[id]}</h1> <h3>{job_desc[id]}</h3>'
+        return HttpResponse(return_html)
+    except:
+        return HttpResponseNotFound('<h2>Page Not Found</h2>')
